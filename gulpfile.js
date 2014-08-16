b2bb1f86e13b1f85614fb8e2ba3dba518a487953
Joe Karayusuf <jkarayusuf@gmail.com>
2014-08-15 22:04:59 -0700

    Setup basic gulp task

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,8 @@
 var gulp = require('gulp');
+var watch = require('gulp-watch');
 
 gulp.task('default', function() {
+  return gulp.src('lib/*.js')
+    .pipe(watch())
+    .pipe(gulp.dest('public'));
 });
