e3db9d2295fe31a80a3fdc1c8ddd8dccd10a2020
Joe Karayusuf <jkarayusuf@gmail.com>
2014-08-16 14:40:35 -0700

    Add gulp-concat

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,9 @@
 var gulp = require('gulp');
 var watch = require('gulp-watch');
+var concat = require('gulp-concat');
 
 gulp.task('default', function() {
-  return gulp.src('lib/*.js')
-    .pipe(watch())
+  return gulp.src('lib/**/*.js')
+    .pipe(concat('application.js'))
     .pipe(gulp.dest('public'));
 });
